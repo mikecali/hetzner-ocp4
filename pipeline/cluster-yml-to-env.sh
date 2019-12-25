ef9adb1152a98afe8ae6889fb80d2f54dbb7467f
Robert Bohne <robert.bohne@redhat.com>
2019-12-25 14:24:20 +0100

    Add missing ANSIBLE_REMOTE_TMP

--- pipeline/cluster-yml-to-env.sh
+++ pipeline/cluster-yml-to-env.sh
@@ -9,6 +9,7 @@ export HETZNER_IP="{{ hetzner_ip }}"
 ' >> $TEMP_DIR/env.j2
 
 export ANSIBLE_LOCALHOST_WARNING=false
+export ANSIBLE_REMOTE_TMP=/tmp
 
 ansible localhost -e @cluster.yml -m template -a "src=$TEMP_DIR/env.j2 dest=cluster.env"
 
